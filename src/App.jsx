571055eb88f4464701a342bd26d2afc198040db4
maheswar <maheswarreddyappojigari@gmail.com>
2025-06-06 15:40:14 +0530

    map added using leaflet from npm

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,42 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import Product from './pages/Product';
-import Pricing from './pages/Pricing';
-import Homepage from './pages/Homepage';
-import PageNotFound from './pages/PageNotFound';
-import PageNav from './components/PageNav'
-import AppLayout from './pages/AppLayout';
+import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+import PageNav from "./components/PageNav";
+import AppLayout from "./pages/AppLayout";
+import City from "./components/City"
+import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import Form from './components/Form'
+import { useEffect, useState } from "react";
+import { CitiesProvider } from "./contexts/CitiesContext";
+
 function App() {
-
+  
   return (
+    <CitiesProvider>
     <BrowserRouter>
-    <PageNav/>
-    <Routes>
-      <Route path='/' element={<Homepage/>}/>
-      <Route path='product' element={<Product/>}/>
-      <Route path='pricing' element={<Pricing/>}/> 
-      <Route path='app' element={<AppLayout/>}/> 
-      <Route path='*' element={<PageNotFound/>}/>
+      {/* <PageNav/> */}
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
 
-    </Routes>
-    
-    
-    </BrowserRouter>
-  )
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<Navigate replace to="cities"/>} />
+         
+          <Route path="cities" element={<CityList />} />
+          <Route path="cities/:id" element={<City />} />
+          <Route path="countries" element={<CountryList />} />
+          <Route path="form" element={<Form/>} />
+        </Route>
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter> </CitiesProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Map.jsx
@@ -0,0 +1,66 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+import styles from "./Map.module.css";
+import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from "react-leaflet";
+import { useEffect, useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
+import { useGeolocation } from "../hooks/useGeoLocation";
+import { useUrlPosition } from "../hooks/useUrlPosition";
+import Button from './Button'
+function Map() {
+  const { cities } = useCities();
+  const [mapPosition, setMapPosition] = useState([40, 12]);
+  const {isLoading:isLoadingPosition,position:getlocationPosition,
+    getPosition
+  }=useGeolocation();
+  const [mapLat,mapLng]=useUrlPosition()
+  useEffect(function(){
+  if(mapLat&&mapLng)  setMapPosition([Number(mapLat),Number(mapLng)]);
+  },[mapLat,mapLng])
+ useEffect(function(){
+if(getlocationPosition) setMapPosition([getlocationPosition.lat,getlocationPosition.lng])
+ },[getlocationPosition])
+  return (
+    <div className={styles.mapContainer}>
+   {!getlocationPosition && <Button type="position" onClick={getPosition}>
+   
+   {isLoadingPosition ? "Loading...": "use your position"}
+   
+    </Button>}
+      <MapContainer center={[mapLat,mapLng]} zoom={6} scrollWheelZoom={true} className={styles.map}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            position={mapPosition}
+            key={city.id}
+          >
+            <Popup>
+            <span>{city.emoji}</span> <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+        <ChangeCenter position={mapPosition}/>
+        <DetectClick/>
+      </MapContainer>
+      
+    </div>
+  );
+}
+function ChangeCenter({position}){
+
+    const map=useMap();
+ 
+    map.setView(position);
+ 
+}
+function DetectClick({position}){
+  
+  const navigate = useNavigate();
+useMapEvents({
+  click:(e)=>navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`)
+})
+
+}
+export default Map;
